1ba1d64c6b2de124173f10fe157bb046fb10a1b5
ashishkekan <ashking201299@gmail.com>
2026-02-24 22:08:24 +0530

    Added album and update code

--- gallery/forms.py
+++ gallery/forms.py
@@ -1,24 +1,38 @@
 # gallery/forms.py
 
 from django import forms
-from .models import MediaFile
+from .models import MediaFile, Album
+
 
 class UploadForm(forms.ModelForm):
     class Meta:
         model = MediaFile
-        fields = ['file', 'category']  # Only file field
+        fields = ["file", "category"]  # Only file field
 
     def clean_file(self):
-        file = self.cleaned_data.get('file')
+        file = self.cleaned_data.get("file")
         if file:
-            ext = file.name.split('.')[-1].lower()
-            if ext in ['jpg', 'jpeg', 'png', 'gif', 'bmp']:
-                self.instance.media_type = 'photo'
-            elif ext in ['mp4', 'avi', 'mov', 'mkv']:
-                self.instance.media_type = 'video'
-            elif ext in ['pdf', 'doc', 'docx', 'txt']:
-                self.instance.media_type = 'document'
+            ext = file.name.split(".")[-1].lower()
+            if ext in ["jpg", "jpeg", "png", "gif", "bmp"]:
+                self.instance.media_type = "photo"
+            elif ext in ["mp4", "avi", "mov", "mkv"]:
+                self.instance.media_type = "video"
+            elif ext in ["pdf", "doc", "docx", "txt"]:
+                self.instance.media_type = "document"
             else:
-                self.instance.media_type = 'document'  # Default
+                self.instance.media_type = "document"  # Default
         return file
-    
+
+
+class AlbumForm(forms.ModelForm):
+    class Meta:
+        model = Album
+        fields = ["name"]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "class": "search-input w-full py-3 px-4 rounded-xl",
+                    "placeholder": "Album Name",
+                }
+            ),
+        }
